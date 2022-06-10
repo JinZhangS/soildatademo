d278b3c452b987793169241575c0ab9265d291bc
JinZ <jinzhang.dsm@gmail.com>
2022-06-10 12:39:08 -0700

    Create documentation file for site_demo deleted DATASET.R

--- R/01_data_to_demo_data.R
+++
@@ -1,6 +0,0 @@
-##Purpose: to poste demo data tables (soil.csv and soil_har.csv) into
-##        the demo data folder
-
-library(tidyverse)
-soil_demo<-read.csv("../Data/03_output/soil_bc_demo.csv")
-soil_demo
--- data-raw/01_data_to_demo_data.R
+++ data-raw/01_data_to_demo_data.R
@@ -1,15 +1,22 @@
 ##Purpose: to poste demo data tables (soil.csv and soil_har.csv) into
 ##        the demo data folder
+## code to prepare `soildatademo` dataset goes here
 
 library(tidyverse)
 library(devtools)
+library(dplyr)
+library(tibble)
 soil_demo<-read.csv("../Data/03_output/soil_bc_demo.csv")
-soil_demo %>%
-  filter(DATA_SOURCE %in% c("BCSIS","kamloops")) %>%
-  count(DATA_SOURCE)
+soil_demo<-soil_demo%>%
+  filter(DATA_SOURCE %in% c("BCSIS"))
 soil_har_demo <-read.csv("../Data/03_output/soil_har_bc_demo.csv")
+soil_har_demo<-soil_har_demo %>%
+  filter(DATA_SOURCE == "BCSIS")
 site_demo<-read.csv("../Data/03_output/site_bc_demo.csv")
+site_demo<-site_demo %>%
+  filter(DATA_SOURCE == "BCSIS")
 
 use_data(soil_demo, overwrite = TRUE)
 use_data(soil_har_demo, overwrite = TRUE)
 use_data(site_demo, overwrite = TRUE)
+
